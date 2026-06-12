c1c4eb27ac76700642524d59ea58f7b31c45e2a2
Goutham S M <goutham.s.m@ericsson.com>
2026-06-12 20:04:39 +0530

    OOPs concepts

---
+++ OOPs_02.java
@@ -0,0 +1,37 @@
+========================= Is-A Relationship/Inheritance =====================
+An Is-A Relationship means one class is a specialized version of another class.
+Achieved by extends keyword and Code resuability
+A java class cannot extend more than one class at a time hence Java doesn't support multiple inheritance in class.
+
+"Every class in java is child class of Object"
+
+class Employee { // Parent class
+	void work() {
+		System.out.println("Employee is working");
+	}
+}
+
+class Manager extends Employee { // Child class
+	void manageTeam() {
+		System.out.println("Manager is managing team");
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Manager mgr = new Manager();
+		mgr.work();
+		mgr.manageTeam();
+		
+		Employee emp = new Employee();
+		emp.manageTeam(); // Error - on parent reference we cannot access child method
+		
+		Employee e = new Manager(); // Parent reference can be used to hold child object;
+		e.work();
+		e. manageTeam(); // Error - on parent reference we cannot access child method
+		
+	}
+}
+
+
+
